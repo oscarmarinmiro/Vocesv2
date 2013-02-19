9000297741cb68e0a74c654e854a0e62db87fb8b
Voices <voices@eeupm.com>
2013-02-19 13:22:53 +0100

    Added requirements.txt file to allow easier deployment.

--- Voces/settings_local.py
+++ Voces/settings_local.py
@@ -4,12 +4,21 @@ DATABASES = {
     # DEV
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'voces',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'NAME': 'voices',                      # Or path to database file if using sqlite3.
+        'USER': 'voices',                      # Not used with sqlite3.
+        'PASSWORD': 'voices2013pp',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '8889',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
+    # DEV
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #    'NAME': 'voces',                      # Or path to database file if using sqlite3.
+    #    'USER': 'root',                      # Not used with sqlite3.
+    #    'PASSWORD': 'root',                  # Not used with sqlite3.
+    #    'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+    #    'PORT': '8889',                      # Set to empty string for default. Not used with sqlite3.
+    #}
     # PRO
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -29,5 +38,6 @@ LOCAL_PATH = normpath(join(dirname(__file__), '..'))
 
 # These are the hostnames as returned by platform.node().
 # If you aren't sure what to put, leave them blank and the error message should tell you which hostname Python sees.
-DEVELOPMENT_HOST = ['minovitch']
+#DEVELOPMENT_HOST = ['minovitch']
+DEVELOPMENT_HOST = ['eeupm']
 PRODUCTION_HOST = []
